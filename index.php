e7136dc13e39bb3ec00a86dc101539e2c406c2b2
Steven Robinett <Steven.robinett@gfcmsu.edu>
2025-02-15 21:20:09 -0700

    Add files via upload
    initial build

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/styles.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <h4>DB Connection Demo</h4>
+
+    <?php
+
+    // phpinfo();
+    echo "Starting PHP<br>";
+    $servername = "localhost:3306"; // or "localhost:3307" for XAMPP
+    $username = "root";
+    $password = "";
+    $dbname = "test";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } else {
+        echo "Connected successfully<br>";
+    }
+
+    $sql = "SELECT * FROM student";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+            foreach ($row as $key => $value) {
+                echo "$key:&nbsp;$value ";
+            }
+            echo "<br><br>";
+        }
+    } else {
+        echo "0 results";
+    }
+    ?>
+</body>
+
+</html>
