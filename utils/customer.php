060caf9f23fb6bd2c9974d873d0f0c8cd80697a0
Steve Robinett <steven.robinett@gfcmsu.edu>
2025-02-19 11:33:23 -0700

    align customer class names with the db names

--- utils/customer.php
+++ utils/customer.php
@@ -2,45 +2,42 @@
 //POPO for the customer table
 class Customer
 {
-    private $customerID;
-    private $firstName;
-    private $lastName;
+    private $cust_id;
+    private $cust_fname;
+    private $cust_lname;
    
 
     public function __construct()
     {
-        $this->customerID = null;
-        $this->firstName = null;
-        $this->lastName = null;
-       
+        ;
     }
 
     public function setCustomerID($customerID)
     {
-        $this->customerID = $customerID;
+        $this->cust_id = $customerID;
     }
     public function setFirstName($firstName)
     {
-        $this->firstName = $firstName;
+        $this->cust_fname = $firstName;
     }
     public function setLastName($lastName)
     {
-        $this->lastName = $lastName;
+        $this->cust_lname = $lastName;
     }
 
     public function getCustomerID()
     {
-        return htmlspecialchars($this->customerID);
+        return htmlspecialchars($this->cust_id);
     }
 
     public function getFirstName()
     {
-        return htmlspecialchars($this->firstName);
+        return htmlspecialchars($this->cust_fname);
     }
 
     public function getLastName()
     {
-        return htmlspecialchars($this->lastName);
+        return htmlspecialchars($this->cust_lname);
     }
 
 }
